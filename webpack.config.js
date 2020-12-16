1fa98d4ef3c7c084fb2e9e077d2afc2c50c5d0f5
Dzmitry Skurat <Dzmitry_Skurat@epam.com>
2020-12-16 15:33:06 +0300

    task1, task2

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,3 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-    mode: 'none',
-    entry: {
-        app: path.join(__dirname, 'src', 'index.tsx')
-    },
-    target: 'web',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: '/node_modules/'
-            }
-        ],
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.html')
-        })
-    ]
-}
+module.exports = process.env.NODE_ENV === 'development'
+    ? require('./webpack.config.dev')
+    : require('./webpack.config.prod');
